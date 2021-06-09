c6d6b5d9094be14a5b2f063733db56cc41dc3c7f
kavya-3103 <84377336+kavya-3103@users.noreply.github.com>
2021-06-09 20:01:10 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import nltk
+import joblib
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+st.title(' Sentiment analysis')
+user_input = st.text_input("Please enter your text")
+nltk.download("vader_lexicon")
+vs = SentimentIntensityAnalyzer()
+score = vs.polarity_scores(user_input)
+st.button("Predict")
+st.title("Result....")
+if score==0:
+   st.write(" ")
+elif score["neg"]!=0:
+   st.title('PREDICTED OUTPUT IS : NEGATIVE') 
+   st.write('AND THE SCORE IS:', score)
+elif score["pos"]!=0:
+   st.title('PREDICTED OUTPUT IS: POSITIVE')
+   st.write(' AND THE SCORE IS:', score)
+elif score["neu"]!=0:
+  st.title('PREDICTED OUTPUT IS : NEUTRAL')
+  st.write(' AND THE SCORE IS:', score)
